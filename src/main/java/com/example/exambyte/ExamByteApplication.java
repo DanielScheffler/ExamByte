b76c3507faa93da083229118cf9ab31e58894db0
Daniel Scheffler <danielscheffler@gmx.de>
2024-10-23 18:54:17 +0200

    Initial Commit

---
+++ src/main/java/com/example/exambyte/ExamByteApplication.java
@@ -0,0 +1,13 @@
+package com.example.exambyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamByteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamByteApplication.class, args);
+	}
+
+}
