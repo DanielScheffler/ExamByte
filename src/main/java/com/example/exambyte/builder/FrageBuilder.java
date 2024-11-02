833404c34d2e19d666f121c3204c1f8731270326
jeremy <quz31kep@uni-duesseldorf.de>
2024-11-02 17:59:30 +0100

    add necessary build methods in the frageBuilder object since updating the frage object

--- src/main/java/com/example/exambyte/builder/FrageBuilder.java
+++ src/main/java/com/example/exambyte/builder/FrageBuilder.java
@@ -1,18 +1,28 @@
 package com.example.exambyte.builder;
 
+import com.example.exambyte.data.FRAGETYP;
 import com.example.exambyte.data.Frage;
 
 public class FrageBuilder {
+    FRAGETYP frageTyp;
     String name;
     String fragestellung;
-    String antwort;
     Integer maxPunktzahl;
+    String antwortmöglichkeiten;
+    String antwort;
 
     public FrageBuilder() {
+        frageTyp = FRAGETYP.FRAGETYP_FREITEXT;
         name = "dummyName";
         fragestellung = "dummyFragestellung";
         antwort = "dummyAntwort";
         maxPunktzahl = 1;
+        antwortmöglichkeiten = "dummyAntwortmöglichkeiten";
+    }
+
+    public FrageBuilder addFragetyp(FRAGETYP frageTyp) {
+        this.frageTyp = frageTyp;
+        return this;
     }
 
     public FrageBuilder addName(String name) {
@@ -30,12 +40,17 @@ public class FrageBuilder {
         return this;
     }
 
+    public FrageBuilder addAntwortmöglichkeiten(String antwortmöglichkeiten){
+        this.antwortmöglichkeiten = antwortmöglichkeiten;
+        return this;
+    }
+
     public FrageBuilder addMaxPunktzahl(Integer maxPunktzahl) {
         this.maxPunktzahl = maxPunktzahl;
         return this;
     }
 
     public Frage build() {
-        return new Frage(name, fragestellung, maxPunktzahl, antwort);
+        return new Frage(frageTyp, name, fragestellung, maxPunktzahl, antwortmöglichkeiten, antwort);
     }
 }
