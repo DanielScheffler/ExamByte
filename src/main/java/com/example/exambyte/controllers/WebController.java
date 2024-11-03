69aae860b966b3ef491586b2d3592ee857455166
jeremy <quz31kep@uni-duesseldorf.de>
2024-11-03 09:25:53 +0100

    inject the wochentestService into the WebController

--- src/main/java/com/example/exambyte/controllers/WebController.java
+++ src/main/java/com/example/exambyte/controllers/WebController.java
@@ -2,15 +2,30 @@ package com.example.exambyte.controllers;
 import com.example.exambyte.builder.FrageBuilder;
 import com.example.exambyte.builder.TestBuilder;
 import com.example.exambyte.data.*;
+import com.example.exambyte.service.WochenTestService;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class WebController {
+
+    private final WochenTestService wochenTestService;
+
+    public WebController(WochenTestService wochenTestService) {
+        this.wochenTestService = wochenTestService;
+
+
+    }
 
     //Controller zur Startseite
     @GetMapping("/")
@@ -23,8 +38,8 @@ public class WebController {
     //Controller für die Erstellung von Tests
     @GetMapping("/testerstellung")
     public String testerstellung(Model model){
-        List<Frage> dummyFragen = initDummyFragen();
-        model.addAttribute("fragen", dummyFragen);
+        //List<Frage> dummyFragen = initDummyFragen();
+        //model.addAttribute("fragen", dummyFragen);
         return "testerstellung";
     }
 
@@ -32,6 +47,21 @@ public class WebController {
     @GetMapping("/testerstellung/fragenerstellung")
     public String fragenerstellung(FrageForm frageForm){
         return "fragenerstellung";
+    }
+
+    @PostMapping("/testerstellung/fragenerstellung/speichern")
+    public String postFragenerstellung(FrageForm frageForm, Model model, @ModelAttribute ArrayList<Frage> fragen,
+                                       RedirectAttributes redirectAttributes){
+        Frage neueFrage = new FrageBuilder()
+                .addFragetyp(frageForm.fragetyp())
+                .addName(frageForm.name())
+                .addFragestellung(frageForm.fragestellung())
+                .addMaxPunktzahl(frageForm.maxPunktzahl())
+                .addAntwortmöglichkeiten(frageForm.antwortmöglichkeiten())
+                .build();
+        fragen.add(neueFrage);
+        redirectAttributes.addFlashAttribute("fragen", fragen);
+        return "redirect:/testerstellung";
     }
 
     //Controller für Tests
