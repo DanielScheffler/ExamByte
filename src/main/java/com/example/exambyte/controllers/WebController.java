a3894435d3d22c18d32c920fe268474135c8be5a
jeremy <quz31kep@uni-duesseldorf.de>
2024-11-03 12:23:23 +0100

    add path variable routing to the handler for the individual wochentests

--- src/main/java/com/example/exambyte/controllers/WebController.java
+++ src/main/java/com/example/exambyte/controllers/WebController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -62,8 +63,16 @@ public class WebController {
     }
 
     //Controller für Tests
-    @GetMapping("/Woche1")
-    public String fragen(){
+    @GetMapping("/{name}")
+    public String fragen(Model model, @PathVariable String name){
+        var maybewWochenTest = wochenTestService.getWochenTests().stream()
+                .filter(w -> w.getName().equals(name))
+                .findAny();
+        if(maybewWochenTest.isPresent()){
+            model.addAttribute("wochenTest", maybewWochenTest.get());
+        } else {
+            model.addAttribute("error", "Keinen Wochen Test mit diesem Namen gefunden.");
+        }
         return "fragen";
     }
 
