cc72e375fbbedad07077c4b68d4176d88ad8b23e
jeremy <quz31kep@uni-duesseldorf.de>
2024-11-03 09:25:16 +0100

    add the necessary Service annotation

--- src/main/java/com/example/exambyte/service/WochenTestService.java
+++ src/main/java/com/example/exambyte/service/WochenTestService.java
@@ -1,4 +1,29 @@
 package com.example.exambyte.service;
 
+import com.example.exambyte.data.WochenTest;
+import com.example.exambyte.repositories.WochenTestRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class WochenTestService {
+
+    WochenTestRepository wochenTestRepository;
+
+    WochenTestService(WochenTestRepository wochenTestRepository) {
+        this.wochenTestRepository = wochenTestRepository;
+    }
+
+    public void addWochenTest(WochenTest wochenTest) {
+        wochenTestRepository.save(wochenTest);
+    }
+
+    public List<WochenTest> getWochenTests() {
+        return wochenTestRepository.findAll();
+    }
+
+    public void removeWochenTest(WochenTest wochenTest) {
+        wochenTestRepository.remove(wochenTest);
+    }
 }
