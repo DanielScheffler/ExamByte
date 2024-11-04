67e4801cd2baa0871ca0c82518f95871b6aa654a
SvenHoops <sven.e.hoops@gmail.com>
2024-11-04 16:26:05 +0100

    help

--- src/main/java/com/example/exambyte/data/FRAGENTYP.java
+++ src/main/java/com/example/exambyte/data/FRAGENTYP.java
@@ -1,4 +1,16 @@
 package com.example.exambyte.data;
 
-public enum FragenTyp {
+public enum FRAGENTYP {
+    FRAGENTYP_FREITEXT ("Freitext"),
+    FRAGENTYP_MULTIPLECHOICE ("Multiple Choice");
+
+
+    String title;
+    FRAGENTYP(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
