33c464cd38eb7561bf1113739e624c8ac7577b70
SvenHoops <sven.e.hoops@gmail.com>
2024-11-06 15:11:07 +0100

    validation in the FrageForm record

--- src/main/java/com/example/exambyte/data/FrageForm.java
+++ src/main/java/com/example/exambyte/data/FrageForm.java
@@ -1,10 +1,14 @@
 package com.example.exambyte.data;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
-public record FrageForm(FRAGENTYP fragentyp,
-                        String name,
-                        String fragestellung,
-                        Integer maxPunktzahl,
+public record FrageForm(@NotNull(message = "darf nicht null sein") FRAGENTYP fragentyp,
+                        @NotBlank(message = "darf nicht leer sein") String name,
+                        @NotBlank(message = "darf nicht leer sein") String fragestellung,
+                        @Min(value = 1 , message = "maxPunktzahl muss größer als 0 sein") @NotNull(message = "darf nicht leer sein") Integer maxPunktzahl,
                         List<AntwortMoeglichkeit> antwortMoeglichkeiten) {
 }
