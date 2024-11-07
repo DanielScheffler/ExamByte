7b6481be6a30355a0d238402d480b5360ed5ee82
Daniel Scheffler <danielscheffler@gmx.de>
2024-11-07 13:44:22 +0100

    AntwortMoeglichkeit as a class instead of as a record

--- src/main/java/com/example/exambyte/data/AntwortMoeglichkeit.java
+++ src/main/java/com/example/exambyte/data/AntwortMoeglichkeit.java
@@ -1,4 +1,37 @@
 package com.example.exambyte.data;
 
-public record AntwortMoeglichkeit(String text, Boolean istWahr) {
+public class AntwortMoeglichkeit {
+    private String text;
+    private boolean istWahr;
+
+    public AntwortMoeglichkeit() {}
+
+    public AntwortMoeglichkeit(String text, boolean istWahr) {
+        this.text = text;
+        this.istWahr = istWahr;
+    }
+
+    public boolean isIstWahr() {
+        return istWahr;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String text() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean istWahr() {
+        return istWahr;
+    }
+
+    public void setIstWahr(boolean istWahr) {
+        this.istWahr = istWahr;
+    }
 }
