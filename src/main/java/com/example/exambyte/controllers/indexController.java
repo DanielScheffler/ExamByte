63df7893abddb6681982d1b11c43b65bdcf9eddb
Daniel Scheffler <danielscheffler@gmx.de>
2024-11-07 13:57:20 +0100

    a Controller indexController

---
+++ src/main/java/com/example/exambyte/controllers/indexController.java
@@ -0,0 +1,28 @@
+package com.example.exambyte.controllers;
+
+import com.example.exambyte.service.WochenTestService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+
+    private final WochenTestService wochenTestService;
+
+    public indexController(WochenTestService wochenTestService) {
+        this.wochenTestService = wochenTestService;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/ExamByte";
+    }
+
+    //Controller zur Startseite
+    @GetMapping("/ExamByte")
+    public String wochenuebersicht(Model model){
+        model.addAttribute("tests", wochenTestService.getWochenTests());
+        return "index";
+    }
+}
