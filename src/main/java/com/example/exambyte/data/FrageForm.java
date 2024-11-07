369337744b04fca36a9e5130612a6303d88dbf3f
Daniel Scheffler <danielscheffler@gmx.de>
2024-11-07 13:43:49 +0100

    a arraylist to AntwortMoeglichkeiten if null

--- src/main/java/com/example/exambyte/data/FrageForm.java
+++ src/main/java/com/example/exambyte/data/FrageForm.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record FrageForm(@NotNull(message = "darf nicht null sein") FRAGENTYP fragentyp,
@@ -11,4 +12,10 @@ public record FrageForm(@NotNull(message = "darf nicht null sein") FRAGENTYP fra
                         @NotBlank(message = "darf nicht leer sein") String fragestellung,
                         @Min(value = 1 , message = "maxPunktzahl muss größer als 0 sein") @NotNull(message = "darf nicht leer sein") Integer maxPunktzahl,
                         List<AntwortMoeglichkeit> antwortMoeglichkeiten) {
+    public FrageForm {
+        if (antwortMoeglichkeiten == null) {
+            antwortMoeglichkeiten = new ArrayList<AntwortMoeglichkeit>();
+        }
+    }
 }
+
