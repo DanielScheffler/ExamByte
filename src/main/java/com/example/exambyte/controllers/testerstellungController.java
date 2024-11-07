12f182b01e00bf8e80a99fa64410296c72c75626
Daniel Scheffler <danielscheffler@gmx.de>
2024-11-07 13:57:46 +0100

    a Controller testerstellungController

---
+++ src/main/java/com/example/exambyte/controllers/testerstellungController.java
@@ -0,0 +1,45 @@
+package com.example.exambyte.controllers;
+
+import com.example.exambyte.builder.TestBuilder;
+import com.example.exambyte.data.Frage;
+import com.example.exambyte.data.STATUS;
+import com.example.exambyte.data.TestForm;
+import com.example.exambyte.data.WochenTest;
+import com.example.exambyte.service.WochenTestService;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class testerstellungController {
+
+    private final WochenTestService wochenTestService;
+
+    public testerstellungController(WochenTestService wochenTestService) {
+        this.wochenTestService = wochenTestService;
+    }
+
+    //Controller für die Erstellung von Tests
+    @GetMapping("/ExamByte/testerstellung")
+    public String testerstellung(TestForm testForm){
+        return "testerstellung";
+    }
+
+    @PostMapping("/ExamByte/testerstellung")
+    public String testerstellung(@Valid TestForm testForm, BindingResult bindingResult, Frage frage){
+        if(bindingResult.hasErrors()){
+            return "testerstellung";
+        }
+        WochenTest neuerTest = new TestBuilder()
+                .addName(testForm.name())
+                .addStartTime(testForm.startTime())
+                .addEndTime(testForm.endTime())
+                .addStatus(STATUS.STATUS_AUSSTEHEND)
+                .build();
+        wochenTestService.addWochenTest(neuerTest);
+        return "redirect:/";
+    }
+
+}
