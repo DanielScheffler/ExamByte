85e824dcb1d44ffa47eed9f07b4b84f14b1444ce
Daniel Scheffler <danielscheffler@gmx.de>
2024-11-11 15:21:38 +0100

    a test if post request at /ExamByte/{testname}/fragenerstellung is possible

--- src/test/java/com/example/exambyte/WebMvcTests/fragenerstellungControllerTest.java
+++ src/test/java/com/example/exambyte/WebMvcTests/fragenerstellungControllerTest.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(fragenerstellungController.class)
@@ -35,4 +36,15 @@ public class fragenerstellungControllerTest {
         mockMvc.perform(get("/ExamByte/woche1/fragenerstellung"))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    @DisplayName("Auf der Seite /ExamByte/{testname}/fragenerstellung, lässt sich ein Post-Request ohne Inhalt senden")
+    void test_2() throws Exception {
+        WochenTest woche1 = new WochenTestTestBuilder()
+                .addName("woche1").build();
+        when(testService.getWochenTests()).thenReturn(List.of(woche1));
+        mockMvc.perform(post("/ExamByte/woche1/fragenerstellung"))
+                .andExpect(status().isOk());
+    }
+
 }
