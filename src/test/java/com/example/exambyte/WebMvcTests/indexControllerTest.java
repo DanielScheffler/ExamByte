327fcf2a0960b267a005d6b29a6b799e3cf30af3
Daniel Scheffler <danielscheffler@gmx.de>
2024-11-12 10:21:41 +0100

    a test to test if / gives a redirect to /ExamByte

--- src/test/java/com/example/exambyte/WebMvcTests/indexControllerTest.java
+++ src/test/java/com/example/exambyte/WebMvcTests/indexControllerTest.java
@@ -1,7 +1,26 @@
 package com.example.exambyte.WebMvcTests;
 
+import com.example.exambyte.controllers.indexController;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(indexControllerTest.class)
+@WebMvcTest(indexController.class)
 public class indexControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Der redirect von / auf /ExamByte funktioniert")
+    void test_1() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/ExamByte"));
+    }
+
 }
