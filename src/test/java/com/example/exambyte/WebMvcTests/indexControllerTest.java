b269d8c828db8617f42166ff28d9740894624369
Daniel Scheffler <danielscheffler@gmx.de>
2024-11-12 10:29:04 +0100

    a test to test if /ExamByte works with Wochentests and returns index.html with a correct model

--- src/test/java/com/example/exambyte/WebMvcTests/indexControllerTest.java
+++ src/test/java/com/example/exambyte/WebMvcTests/indexControllerTest.java
@@ -1,6 +1,9 @@
 package com.example.exambyte.WebMvcTests;
 
 import com.example.exambyte.controllers.indexController;
+import com.example.exambyte.data.FRAGENTYP;
+import com.example.exambyte.data.STATUS;
+import com.example.exambyte.data.WochenTest;
 import com.example.exambyte.service.WochenTestService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,7 +12,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -39,5 +44,17 @@ public class indexControllerTest {
         mockMvc.perform(get("/ExamByte"))
                 .andExpect(view().name("index"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("")
+    void  test_3() throws Exception {
+        List<WochenTest> testWochenTests = List.of(new WochenTest(new ArrayList<>(), LocalDateTime.MIN, LocalDateTime.MAX, "testWochenTest1", STATUS.STATUS_AUSSTEHEND));
+        when(wochenTestService.getWochenTests()).thenReturn(testWochenTests);
+        mockMvc.perform(get("/ExamByte"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attributeExists("tests"))
+                .andExpect(model().attribute("tests", testWochenTests));
     }
 }
