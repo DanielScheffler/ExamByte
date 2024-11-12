7ff93ee652cbe635ba41583e276b1d6722be3e31
Daniel Scheffler <danielscheffler@gmx.de>
2024-11-12 10:22:56 +0100

    a test to test if /ExamByte works without Wochentests and returns index.html

--- src/test/java/com/example/exambyte/WebMvcTests/indexControllerTest.java
+++ src/test/java/com/example/exambyte/WebMvcTests/indexControllerTest.java
@@ -1,11 +1,17 @@
 package com.example.exambyte.WebMvcTests;
 
 import com.example.exambyte.controllers.indexController;
+import com.example.exambyte.service.WochenTestService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -15,6 +21,9 @@ public class indexControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
+    WochenTestService wochenTestService;
+
     @Test
     @DisplayName("Der redirect von / auf /ExamByte funktioniert")
     void test_1() throws Exception {
@@ -23,4 +32,12 @@ public class indexControllerTest {
                 .andExpect(redirectedUrl("/ExamByte"));
     }
 
+    @Test
+    @DisplayName("Der Get Request auf /ExamByte funktioniert ohne Wochentests")
+    void test_2() throws Exception {
+        when(wochenTestService.getWochenTests()).thenReturn(new ArrayList<>());
+        mockMvc.perform(get("/ExamByte"))
+                .andExpect(view().name("index"))
+                .andExpect(status().isOk());
+    }
 }
