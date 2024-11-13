074122ceb3c89029fd7d54931d61c43405516d9a
SvenHoops <sven.e.hoops@gmail.com>
2024-11-13 15:09:45 +0100

    ErrorHandleAdvice with basic Methods for ExceptionHandling so that every Controller knows how to deal with Exception

--- src/main/java/com/example/exambyte/controllers/ErrorHandleAdvice.java
+++ src/main/java/com/example/exambyte/controllers/ErrorHandleAdvice.java
@@ -1,4 +1,39 @@
 package com.example.exambyte.controllers;
 
+import com.example.exambyte.exceptions.FrageNichtGefundenException;
+import com.example.exambyte.exceptions.TestNichtGefundenException;
+import com.example.exambyte.exceptions.TestnameExistiertBereitsException;
+import com.example.exambyte.exceptions.ZeitraumUngueltigException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class ErrorHandleAdvice {
+
+    @ExceptionHandler(FrageNichtGefundenException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleFrageNichtGefundenException(FrageNichtGefundenException e) {
+        return e.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(TestNichtGefundenException.class)
+    public String handleTestNichtGefundenException(TestNichtGefundenException e) {
+        return e.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ZeitraumUngueltigException.class)
+    public String handleZeitraumUngueltigException(ZeitraumUngueltigException e) {
+        return e.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(TestnameExistiertBereitsException.class)
+    public String handleTestnameExistiertBereitsException(TestnameExistiertBereitsException e) {
+        return e.getMessage();
+    }
+
 }
