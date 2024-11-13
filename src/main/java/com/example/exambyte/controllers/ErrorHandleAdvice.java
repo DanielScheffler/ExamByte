9d299538d222f82faa0870106ab8457f02e95b73
SvenHoops <sven.e.hoops@gmail.com>
2024-11-13 15:06:45 +0100

    FrageNichtGefundenException

---
+++ src/main/java/com/example/exambyte/controllers/ErrorHandleAdvice.java
@@ -0,0 +1,4 @@
+package com.example.exambyte.controllers;
+
+public class ErrorHandleAdvice {
+}
---
+++ src/main/java/com/example/exambyte/exceptions/FrageNichtGefundenException.java
@@ -0,0 +1,4 @@
+package com.example.exambyte.exceptions;
+
+public class FrageNichtGefundenException extends RuntimeException {
+}
