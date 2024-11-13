b9e15cc46b47bd75133fc7307da70237fa8a8d7a
SvenHoops <sven.e.hoops@gmail.com>
2024-11-13 15:28:56 +0100

    FragennameExistiertBereitsException

---
+++ src/main/java/com/example/exambyte/exceptions/FragennameExistiertBereitsException.java
@@ -0,0 +1,7 @@
+package com.example.exambyte.exceptions;
+
+public class FragennameExistiertBereitsException extends RuntimeException {
+    public FragennameExistiertBereitsException(String message) {
+        super(message);
+    }
+}
