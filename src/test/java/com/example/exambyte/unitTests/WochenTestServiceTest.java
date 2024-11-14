0f050fb191b67714939d24b7706aea07ac9a220e
SvenHoops <sven.e.hoops@gmail.com>
2024-11-14 14:12:01 +0100

    WochenTestServiceTest to test if you can add a Test via WochenTestService

--- src/test/java/com/example/exambyte/unitTests/WochenTestServiceTest.java
+++ src/test/java/com/example/exambyte/unitTests/WochenTestServiceTest.java
@@ -1,4 +1,32 @@
 package com.example.exambyte.unitTests;
 
+import com.example.exambyte.data.WochenTest;
+import com.example.exambyte.repositories.WochenTestRepository;
+import com.example.exambyte.service.WochenTestService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import static org.mockito.Mockito.*;
+
 public class WochenTestServiceTest {
+
+
+    @Test
+    @DisplayName("Über WochenTestService kann WochenTest hinzugefügt werden")
+    void test_1(){
+        //Arrange
+        WochenTestRepository wochenTestRepository = mock(WochenTestRepository.class);
+        WochenTestService wochenTestService = new WochenTestService(wochenTestRepository);
+        WochenTest wochenTest = new WochenTestTestBuilder()
+                .addName("Wochentest1")
+                .addStartTime(LocalDateTime.MIN)
+                .addEndTime(LocalDateTime.MAX)
+                .build();
+        //Act
+        wochenTestService.addWochenTest(wochenTest);
+        //Assert
+        verify(wochenTestRepository).save(wochenTest);
+    }
+
 }
