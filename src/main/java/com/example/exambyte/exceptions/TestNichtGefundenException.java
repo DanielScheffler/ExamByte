ebc56f8e4d517ae7a025a122385fc19295bd44b1
Daniel Scheffler <danielscheffler@gmx.de>
2024-11-14 13:46:46 +0100

    add error message to exceptionsconstructor

--- src/main/java/com/example/exambyte/exceptions/TestNichtGefundenException.java
+++ src/main/java/com/example/exambyte/exceptions/TestNichtGefundenException.java
@@ -1,4 +1,7 @@
 package com.example.exambyte.exceptions;
 
 public class TestNichtGefundenException extends RuntimeException {
+    public TestNichtGefundenException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/example/exambyte/exceptions/TestnameExistiertBereitsException.java
+++ src/main/java/com/example/exambyte/exceptions/TestnameExistiertBereitsException.java
@@ -1,4 +1,7 @@
 package com.example.exambyte.exceptions;
 
 public class TestnameExistiertBereitsException extends RuntimeException {
+    public TestnameExistiertBereitsException(String message) {
+        super(message);
+    }
 }
