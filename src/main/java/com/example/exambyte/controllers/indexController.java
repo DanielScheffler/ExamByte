b80e83e57ecf620e5c99bfa58f35d4d6e45b783c
jeremy <quz31kep@uni-duesseldorf.de>
2024-11-28 10:24:24 +0100

    add role dependent template rendering to the index.html and indexController

--- src/main/java/com/example/exambyte/controllers/indexController.java
+++ src/main/java/com/example/exambyte/controllers/indexController.java
@@ -2,6 +2,8 @@ package com.example.exambyte.controllers;
 
 import com.example.exambyte.service.WochenTestService;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +25,8 @@ public class indexController {
     //Controller zur Startseite
     @GetMapping("/ExamByte")
     @Secured("ROLE_STUDENT")
-    public String wochenuebersicht(Model model){
+    public String wochenuebersicht(Model model, @AuthenticationPrincipal OAuth2User principal){
+        model.addAttribute("authorities", principal.getAuthorities().toString());
         model.addAttribute("tests", wochenTestService.getWochenTests());
         return "index";
     }
