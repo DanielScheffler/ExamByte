863d4f09fe2bec62849727f9cbaa05c76aa8efd2
jeremy <quz31kep@uni-duesseldorf.de>
2024-11-28 07:27:38 +0100

    add OAuth2 roles to wochentestControllerTest

--- src/test/java/com/example/exambyte/WebMvcTests/wochentestControllerTest.java
+++ src/test/java/com/example/exambyte/WebMvcTests/wochentestControllerTest.java
@@ -1,9 +1,11 @@
 package com.example.exambyte.WebMvcTests;
 
 import com.example.exambyte.builder.FrageBuilder;
+import com.example.exambyte.configuration.MethodSecurityConfig;
 import com.example.exambyte.controllers.wochentestController;
 import com.example.exambyte.data.Frage;
 import com.example.exambyte.data.WochenTest;
+import com.example.exambyte.helper.WithMockOAuth2User;
 import com.example.exambyte.service.WochenTestService;
 import com.example.exambyte.unitTests.WochenTestTestBuilder;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
@@ -18,6 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(wochentestController.class)
+@Import(MethodSecurityConfig.class)
 public class wochentestControllerTest {
 
     @Autowired
@@ -27,43 +31,38 @@ public class wochentestControllerTest {
     WochenTestService wochenTestService;
 
     @Test
-    @DisplayName("Wenn ein leerer Wochentest 'dummyTest' gespeichert ist, dann ist die Route /ExamByte/dummyTest vorhanden.")
+    @DisplayName("Wenn ein leerer Wochentest 'dummyTest' gespeichert ist, dann ist die Route /ExamByte/dummyTest für Studenten vorhanden.")
+    @WithMockOAuth2User(roles={"STUDENT"})
     void test_1() throws Exception {
 
-        //Test stubben
         String testName = "dummyTest";
         WochenTest wochenTestStub = new WochenTestTestBuilder()
                 .addName(testName)
                 .build();
 
-        //WochenTestService mocken
         when(wochenTestService.getWochenTest(testName)).thenReturn(wochenTestStub);
 
-        //Assertion
         mockMvc.perform(get("/ExamByte/dummyTest")).andExpect(status().isOk());
     }
 
     @Test
     @DisplayName("Wenn ein Wochentest 'dummyTest' mit der Frage 'dummyFrage' gespeichert ist, " +
-            "dann ist die Route /ExamByte/dummyTest/dummyFrage vorhanden.")
+            "dann ist die Route /ExamByte/dummyTest/dummyFrage für Studenten vorhanden.")
+    @WithMockOAuth2User(roles={"STUDENT"})
     void test_2() throws Exception {
 
-        //Frage stubben
         Frage frageStub = new FrageBuilder()
                 .addName("dummyFrage")
                 .build();
 
-        //Test stubben
         String testName = "dummyTest";
         WochenTest wochenTestStub = new WochenTestTestBuilder()
                 .addName(testName)
                 .addFrage(frageStub)
                 .build();
 
-        //Service mocken
         when(wochenTestService.getWochenTest(testName)).thenReturn(wochenTestStub);
 
-        //Assertion
         mockMvc.perform(get("/ExamByte/dummyTest/dummyFrage")).andExpect(status().isOk());
     }
 }
