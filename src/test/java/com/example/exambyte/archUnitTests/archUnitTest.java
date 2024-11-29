3d55f64ce50bedb990a066774b31bc482e8424c0
jeremy <quz31kep@uni-duesseldorf.de>
2024-11-29 18:57:12 +0100

    add onion test

--- src/test/java/com/example/exambyte/archUnitTests/archUnitTest.java
+++ src/test/java/com/example/exambyte/archUnitTests/archUnitTest.java
@@ -5,8 +5,21 @@ import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.library.Architectures.onionArchitecture;
 
 @AnalyzeClasses(packagesOf = ExamByteApplication.class, importOptions = ImportOption.DoNotIncludeTests.class)
 public class archUnitTest {
     private final JavaClasses classes = new ClassFileImporter().importClasses(ExamByteApplication.class);
+
+    @ArchTest
+    ArchRule onionTest = onionArchitecture()
+            .applicationServices("..application.service..")
+            .domainServices("..domain.service..")
+            .domainModels("..domain.model..")
+            .adapter("web", "..controllers..")
+            .adapter("config", "..configuration..")
+            .adapter("persistence", "..repositories..");
 }
