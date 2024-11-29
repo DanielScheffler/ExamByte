dead0e2a3994d5a10bb2769ad1fb8bd3be566450
jeremy <quz31kep@uni-duesseldorf.de>
2024-11-29 08:52:05 +0100

    remove unnecessary imports

--- src/main/java/com/example/exambyte/ExamByteApplication.java
+++ src/main/java/com/example/exambyte/ExamByteApplication.java
@@ -1,17 +1,7 @@
 package com.example.exambyte;
 
-import com.example.exambyte.builder.FrageBuilder;
-import com.example.exambyte.builder.WochenTestBuilder;
-import com.example.exambyte.domain.model.*;
-import com.example.exambyte.service.WochenTestService;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @SpringBootApplication
 public class ExamByteApplication {
