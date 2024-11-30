9f1a81c5d0f9ba46f1c93d60196fed4bb9e9872b
jeremy <quz31kep@uni-duesseldorf.de>
2024-11-30 08:51:58 +0100

    add rule that all controller fields should be private

--- src/test/java/com/example/exambyte/archUnitTests/archUnitTest.java
+++ src/test/java/com/example/exambyte/archUnitTests/archUnitTest.java
@@ -5,8 +5,10 @@ import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
+import org.springframework.stereotype.Controller;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
 import static com.tngtech.archunit.library.Architectures.onionArchitecture;
 import static com.tngtech.archunit.library.plantuml.rules.PlantUmlArchCondition.Configuration.consideringOnlyDependenciesInDiagram;
 import static com.tngtech.archunit.library.plantuml.rules.PlantUmlArchCondition.adhereToPlantUmlDiagram;
@@ -28,5 +30,13 @@ public class archUnitTest {
             ExamByteApplication.class.getResource("/architecture.puml"),
             consideringOnlyDependenciesInDiagram()
     ));
+
+    @ArchTest
+    ArchRule allFieldsInControllersShouldBePrivate = fields()
+            .that()
+            .areDeclaredInClassesThat()
+            .areAnnotatedWith(Controller.class)
+            .should()
+            .bePrivate();
 
 }
